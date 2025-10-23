35c1b1152cb06fb3b7915595de16599c6d7d9001
Benyamin Shoraki <benyaminchor@gmail.com>
2025-10-23 12:22:54 +0300

    add main.py: run func

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+from game_logic.game import init_game, play_round
 
 
-
-
-
-
-
-
-
-
-
+def run():
+    players_dict: dict = init_game()
+    player_1 = players_dict["player_1"]
+    player_2 = players_dict["player_2"]
+    while player_1["hand"] != [] and player_2["hand"] != []:
+        play_round(players_dict["player_1"], players_dict["player_2"])
+    winner = {}
+    if len(player_1["won_pile"]) > len(player_2["won_pile"]):
+        winner = player_1
+        # print(f"{player_1["name"]} wins! With {player_1["won_pile"]} cards")
+    if len(player_1["won_pile"]) < len(player_2["won_pile"]):
+        winner = player_1
+        # print(f"{player_2["name"]} wins! With {player_1["won_pile"]} cards")
+    if winner != {}:
+        print(f"{winner["name"]} wins! With {len(winner["won_pile"])} cards")
+    else:
+        print("Draw - Both win")
 
 
 if __name__ == "__main__":
-    pass
+    run()
+
+
+
