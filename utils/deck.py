ade0df755936e573cb15147fd9ad1035dfa049e7
Benyamin Shoraki <benyaminchor@gmail.com>
2025-10-23 11:17:27 +0300

    add utils:deck.py: create_card func

--- utils/deck.py
+++ utils/deck.py
@@ -1,12 +1,4 @@
 
 def create_card(rank:str,suite:str) -> dict:
-    pass
-
-def compare_cards(p1_card:dict, p2_card:dict) -> str:
-    pass
-
-def create_deck() -> list[dict]:
-    pass
-
-def shuffle(deck:list[dict]) -> list[dict]:
-    pass
+    rank_dict = {'A':14, '2':2, '3':3, '4':4, '5':5,'6':6, '7':7, '8':8, '9':9, '10':10, 'J':11, 'Q':12, 'K':13}
+    return {"rank":rank, "suite":suite, "value":rank_dict[rank]}
