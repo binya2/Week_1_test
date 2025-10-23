8d0c21cd0aa3fd1ffc62f035f12eb4baa0650335
Benyamin Shoraki <benyaminchor@gmail.com>
2025-10-23 11:50:20 +0300

    add game_logic: game.py: init_game func

--- game_logic/game.py
+++ game_logic/game.py
@@ -1,2 +1,14 @@
+from utils.deck import shuffle, create_deck
+
+
 def create_player(name:str = 'AI') -> dict:
     return {"name":name, "hand":[], "won_pile":[]}
+
+
+def init_game()->dict:
+    player1 = create_player(name='P1')
+    player2 = create_player(name='P2')
+    new_shuffl_deck =  shuffle(create_deck())
+    player1["hand"] = new_shuffl_deck[:26]
+    player2["hand"] = new_shuffl_deck[26:]
+
